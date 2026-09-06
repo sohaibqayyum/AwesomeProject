b26d627e348f033f67f4e9366ee04b50683846f2
sohaibqayyum <145961633+sohaibqayyum@users.noreply.github.com>
2026-09-06 15:21:31 +0200

    Add SMS role response service

---
+++ gateway/src/main/java/com/sohaib/callbridge/gateway/RespondViaMessageService.kt
@@ -0,0 +1,9 @@
+package com.sohaib.callbridge.gateway
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class RespondViaMessageService:Service(){
+    override fun onBind(intent:Intent?):IBinder?=null
+}
