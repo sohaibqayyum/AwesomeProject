8994d038bb66c0b048f39022d2fc36f38e29a7d9
sohaibqayyum <145961633+sohaibqayyum@users.noreply.github.com>
2026-09-06 11:36:00 +0200

    Add controller module configuration

---
+++ controller/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android {
+    namespace = "com.sohaib.callbridge.controller"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.sohaib.callbridge.controller"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+}
